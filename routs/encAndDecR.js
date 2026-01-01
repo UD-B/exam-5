f614ad2ffad02b8f4fbed67c95b5b302ce55a620
UD <yudibarak770@gmail.com>
2026-01-01 12:52:08 +0200

    yet to finish encryption flow

--- routs/encAndDecR.js
+++ routs/encAndDecR.js
@@ -1,8 +1,10 @@
 import express from 'express'
+import { encAuth } from '../mdlwrs/encAuth.js'
+
 
 const router = express.Router()
 
-// router.post("/encrypt",)
-// router.post("/decrypt",)
+router.post("/encrypt", encAuth, )
+router.post("/decrypt",)
 
 export default router
